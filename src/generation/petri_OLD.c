e9c44ae55399d05ac0090af218dbadc1fdfe810b
Gabriel Eudes <gaeudes@student.42.fr>
2026-07-10 21:04:29 +0200

    Implementing it

--- src/generation/petri_OLD.c
+++ src/generation/petri_OLD.c
@@ -2,69 +2,58 @@
 #include "hashtable.h"
 #include "vector.h"
 #include "fenwick_tree.h"
-#include <math.h>
 
-typedef struct {
-	size_t	weight;
-	uint64_t	xOrigin;
-	uint64_t	yOrigin;
-	t_vec	*vec;
-	t_ht	*ht;
-}	t_cluster;
-
-typedef struct {
+typedef struct s_petriPoint {
 	size_t	x;
 	size_t	y;
-	t_cluster	*origin;
-}	t_point;
+}	t_petriPoint;
 
 
 typedef struct s_petri {
-	t_vec	*vStart;
-	t_fTree	weightStarts;
+	t_vec	*vec;
+	t_ht	*ht;
 }	t_petri;
-
 
 enum {	UP, LEFT, DOWN, RIGHT	};
 
-size_t	clusterHash(const void *rPtr) {
-	const t_point	*data = rPtr;
+size_t	petriHash(const void *rPtr) {
+	const t_petriPoint	*data = rPtr;
 	
 	return (data->x * 0x1516075A72F05 + data->y * 971);
 }
 
-void	*clusterDup(const void *_toDup) {
-	const t_point *toDup = _toDup;
-	t_point	*res = malloc(sizeof(t_point));
+void	*petriDup(const void *_toDup) {
+	const t_petriPoint *toDup = _toDup;
+	t_petriPoint	*res = malloc(sizeof(t_petriPoint));
 
-	if (res)
-		*res = *toDup;
+	if (!res)	return (NULL);
+	*res = *toDup;
 	return (res);
 }
 
-bool	clusterCmp(const void *d1, const void *d2) {
-	const t_point *p1 = d1;
-	const t_point *p2 = d2;
+bool	petriCmp(const void *d1, const void *d2) {
+	const t_petriPoint *p1 = d1;
+	const t_petriPoint *p2 = d2;
 	return (!(p1->x == p2->x && p1->y == p2->y));
 }
 
-void	*clusterAdd(t_cluster *cluster, t_point p) {
-	void	*search = ht_get(cluster->ht, &p);
+void	*petriAdd(t_petri *petri, t_petriPoint p) {
+	void	*search = ht_get(petri->ht, &p);
 
 	if (!search) {
-		search = ht_add(cluster->ht, &p);
-		vec_add(cluster->vec, search);
+		search = ht_add(petri->ht, &p);
+		vec_add(petri->vec, search);
 	}
 	return (search);
 }
 
-void	clusterRm(t_cluster *cluster, const size_t index, const void *item) {
+void	petriRm(t_petri *petri, const size_t index, const void *item) {
 	if (item != NULL) {
-		ht_rm(cluster->ht, item);
+		ht_rm(petri->ht, item);
 	} else {
-		ht_rm(cluster->ht, vec_get(cluster->vec, index));
+		ht_rm(petri->ht, vec_get(petri->vec, index));
 	}
-	vec_rm(cluster->vec, index);
+	vec_rm(petri->vec, index);
 }
 
 __always_inline
@@ -133,74 +122,83 @@ uint8_t	_choseOnePossibility(const uint8_t poss, const uint8_t nPoss) {
 }
 
 __always_inline
-void	_joinPoint(t_cluster *cluster, const t_point *node, const uint8_t choice, uint8_t *tab[]) {
+void	_joinPointColor(t_art *art, const t_petriPoint *node, const int dX, const int dY) {
+	const size_t	sX = node->x * 2;
+	const size_t	sY = node->y * 2;
+
+	if (art->color == CLR_GRADIENT && 0) {
+		art->arrClr[sY + dY][sX + dX] = seededNewColor(art->arrClr[sY][sX], &art->clrSetting);
+		art->arrClr[sY + 2 * dY][sX + 2 * dX] = seededNewColor(art->arrClr[sY + dY][sX + dX], &art->clrSetting);
+	}
+}
+
+__always_inline
+void	_joinPoint(t_petri *petri, const t_petriPoint *node, const uint8_t choice, uint8_t *tab[], t_art *art) {
 	const size_t	bX = node->x;
 	const size_t	bY = node->y * 2;
 
 	if (choice == UP) {
 		tab[bY - 1][bX / 8] |= MASK(bX % 8);
-		clusterAdd(cluster, (t_point){bX, node->y - 1, node->origin});
+		petriAdd(petri, (t_petriPoint){bX, node->y - 1});
+		_joinPointColor(art, node, 0, -1);
 	} else if (choice == DOWN) {
 		tab[bY + 1][bX / 8] |= MASK(bX % 8);
-		clusterAdd(cluster, (t_point){bX, node->y + 1, node->origin});
+		petriAdd(petri, (t_petriPoint){bX, node->y + 1});
+		_joinPointColor(art, node, 0, 1);
 	} else if (choice == LEFT) {
 		const size_t	nX = bX - 1;
 		tab[bY][nX / 8] |= MASK(nX % 8);
-		clusterAdd(cluster, (t_point){nX, node->y, node->origin});
+		petriAdd(petri, (t_petriPoint){nX, node->y});
+		_joinPointColor(art, node, -1, 0);
 	} else if (choice == RIGHT) {
 		const size_t	nX = bX;
 		tab[bY][nX / 8] |= MASK(nX % 8);
-		clusterAdd(cluster, (t_point){bX + 1, node->y, node->origin});
+		petriAdd(petri, (t_petriPoint){bX + 1, node->y});
+		_joinPointColor(art, node, 1, 0);
 	} else {
 		printf("Erorr\n");
 	}
 }
 
-void	_initPetri(t_petri *petri, t_art *art) {
-	petri->vStart = vec_create(sizeof(t_cluster));
-	fTree_create(&petri->weightStarts);
-	
-	{
-		const t_startList	*startL = art->starts;
-		for (size_t i = 0; i < startL->n; ++i) {
-			t_cluster		cluster = {.weight = 3, .xOrigin = startL->lStart[i].x / 2, .yOrigin = startL->lStart[i].y / 2};
-			const t_point	sPoint = {startL->lStart[i].x / 2, startL->lStart[i].y / 2, &cluster};
-
-			cluster.ht = ht_create(sqrt(art->height * art->width), clusterHash, clusterDup, clusterCmp, free);
-			cluster.vec = vec_create(sizeof(t_point));
-
-			vec_add(petri->vStart, &cluster);
-			fTree_append(&petri->weightStarts, cluster.weight, &cluster);
-		}
-	}
-}
-
 void	genTabPetri(t_art *tab) {
 	t_petri	petri = {};
-	_initPetri(&petri, tab);
-	size_t	totalWeight = petri.weightStarts.BIT[petri.weightStarts.cap - 1];
+
+	petri.ht = ht_create((tab->width * tab->height) / 8 + 4,
+				petriHash, petriDup, petriCmp, free);
+	petri.vec = vec_create(sizeof(t_petriPoint));
 	
-	while (totalWeight != 0) {
-		const uint64_t	index = fTree_getIndex(&petri.weightStarts, aRand(totalWeight));
-		const t_cluster	*cluster = petri.weightStarts.val[index].data;
-		const size_t	rItem = aRand(cluster->ht->nItems);
-		t_point	node = *(t_point *)vec_get(cluster->vec, rItem);
+	if (!petri.ht || !petri.vec) {
+		vec_destroy(petri.vec);
+		ht_destroy(petri.ht);
+		return ;
+	}
+	{
+		const t_startList	*startL = tab->starts;
+		for (size_t i = 0; i < startL->n; ++i) {
+			const t_petriPoint	p = {startL->lStart[i].x / 2, startL->lStart[i].y / 2};
+			petriAdd(&petri, p);
+			if (tab->color == CLR_GRADIENT && 0)
+				tab->arrClr[p.y * 2][p.x * 2] = newColor(tab->clrSetting.min, tab->clrSetting.max);
+		}
+	}
+	while (petri.ht->nItems != 0) {
+		const size_t	rItem = aRand(petri.ht->nItems);
+		t_petriPoint	node = *(t_petriPoint *)vec_get(petri.vec, rItem);
 		const uint8_t	poss = getPossibility(tab->arr, tab->width, tab->height, node.x, node.y);
 		const uint8_t	nPoss = __builtin_popcount(poss);
 		
 		if (nPoss == 0) {
-			clusterRm(cluster, rItem, &node);
+			petriRm(&petri, rItem, &node);
 			continue;
 		} else {
 			const int  choice = _choseOnePossibility(poss, nPoss);
 			
-			_joinPoint(cluster, &node, choice, tab->arr);
+			_joinPoint(&petri, &node, choice, tab->arr, tab);
 			if (nPoss == 1) {
-				clusterRm(cluster, rItem, &node);
+				petriRm(&petri, rItem, &node);
 			}
 		}
 	}
-	// free hash and vec from clusters
-	fTree_destroy(&petri.weightStarts);
-	vec_destroy(petri.vStart);
+	vec_destroy(petri.vec);
+	ht_destroy(petri.ht);
 }
